0889f5ae92f6b8f2c2f9242cc5c97a7fe3e07181
SammyFerreiraa <sammyfe2021@gmail.com>
2025-01-19 20:25:39 -0300

    :sparkles: feat: configure database connection and enable foreign key support

--- app/database.py
+++ app/database.py
@@ -1,11 +1,17 @@
 from sqlmodel import create_engine, SQLModel, Session
+import sqlite3
 from dotenv import load_dotenv
+from sqlalchemy import event, Engine
+import logging
 import os
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./project_manager.db")
-engine = create_engine(DATABASE_URL, echo=True)
+# Configurar o logger
+logging.basicConfig()
+logging.getLogger("sqlalchemy.engine").setLevel(logging.INFO)
+
+engine = create_engine(os.getenv("DATABASE_URL"))
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
@@ -13,3 +19,10 @@ def create_db_and_tables():
 def get_session():
     with Session(engine) as session:
         yield session
+
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    if type(dbapi_connection) is sqlite3.Connection:  # somente para o SQLite
+       cursor = dbapi_connection.cursor()
+       cursor.execute("PRAGMA foreign_keys=ON")
+       cursor.close()
--- app/main.py
+++ app/main.py
@@ -1,16 +1,18 @@
 from fastapi import FastAPI
 from app.database import create_db_and_tables
+from contextlib import asynccontextmanager
 from app.project.project_routes import router as project_routes
 from app.collaborator.collaborator_routes import router as collaborator_routes
 from app.task.task_routes import router as task_routes
 
-app = FastAPI()
 
-@app.on_event("startup")
-def on_startup():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     create_db_and_tables()
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(collaborator_routes)
 app.include_router(project_routes)
 app.include_router(task_routes)
-
